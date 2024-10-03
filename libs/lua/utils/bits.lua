d165f4adb9d46fff11c71e1f25b12bf0150a13f4
sepi <me@sepi.me>
2024-10-03 23:34:37 +0200

    rewrote the bits module

--- libs/lua/utils/bits.lua
+++ libs/lua/utils/bits.lua
@@ -1,80 +1,234 @@
----@param file file*
----@return string
-local read_all = function (file)
-  return file:read("a")
-end
+BYTE_SIZE = 1
+SHORT_SIZE = BYTE_SIZE * 2
+INTEGER_SIZE = SHORT_SIZE * 2
+LONG_SIZE = INTEGER_SIZE * 2
+FLOAT_SIZE = INTEGER_SIZE
+DOUBLE_SIZE = LONG_SIZE
 
----@param file file*
----@return any
-local write_all = function (file, data)
-  return file:write(data)
-end
+---========================================
+--- READER
+---========================================
 
----@param file file*
----@param size integer
----@return any
-local read_buf = function (file, size)
-  return file:read(size)
-end
-
----@param file file*
----@param size integer
----@return string
-local read_chars = function (file, size)
-  local code = "c"..size
-  return string.pack(code, file:read(size))
-end
-
----@param file file*
----@param str string
----@param size integer
-local write_chars = function (file, str, size)
-  local code = "c"..size
-  file:write(string.pack(code, str))
-end
-
----@param file file*
----@param size integer
----@return string
-local read_c_str = function (file, size)
-  return string.unpack("z", file:read(size))
-end
-
----@param file file*
----@param size integer
+--- read_byte
+---@param f file*
+---@param signed boolean|nil
 ---@return integer
-local read_i32 = function (file, size)
-  return string.unpack("=i", file:read(size))
+local read_byte = function (f, signed)
+  local fmt = "=B"
+  if signed then fmt = "=b" end
+  return string.unpack(fmt, f:read(BYTE_SIZE))
 end
 
----@param file file*
----@param size integer
-local write_i32 = function (file, size)
-  file:write(string.pack("=i", size))
+--- read_short
+--- @param f file*
+--- @param signed boolean|nil
+--- @returns integer
+local read_short = function (f, signed)
+  local fmt = "=H"
+  if signed then fmt = "=h" end
+  return string.unpack(fmt, f:read(SHORT_SIZE))
 end
 
----@param file file*
----@param size integer
----@return integer
-local read_u8 = function (file, size)
-  return string.unpack("=B", file:read(size))
+--- read_integer
+--- @param f file*
+--- @param signed boolean|nil
+--- @returns integer
+local read_integer = function (f, signed)
+  local fmt = "=I"
+  if signed then fmt = "=i" end
+  return string.unpack(fmt, f:read(INTEGER_SIZE))
 end
 
----@param file file*
----@param size integer
-local write_u8 = function (file, size)
-  file:write(string.pack("=B", size))
+--- read_long
+--- @param f file*
+--- @param signed boolean|nil
+--- @returns integer
+local read_long = function (f, signed)
+  local fmt = "=L"
+  if signed then fmt = "=l" end
+  return string.unpack(fmt, f:read(LONG_SIZE))
 end
+
+--- read_float
+--- @param f file*
+--- @param double boolean|nil
+--- @returns number
+local read_float = function (f, double)
+  if double then
+    return string.unpack("=d", f:read(DOUBLE_SIZE))
+  end
+  return string.unpack("=f", f:read(FLOAT_SIZE))
+end
+
+--- read_size
+--- @param f file*
+--- @returns integer
+local read_size = function (f)
+  local fmt = "=T"
+  return string.unpack(fmt, f:read(INTEGER_SIZE))
+end
+
+--- read_string
+--- @param f file*
+--- @param size integer
+--- @returns string
+local read_string = function (f, size)
+  local fmt = "c" .. size
+  return string.unpack(fmt, f:read(size))
+end
+
+--- read_cstring
+--- @param f file*
+--- @param size integer
+--- @returns string
+local read_cstring = function (f, size)
+  local fmt = "z"
+  return string.unpack(fmt, f:read(size))
+end
+
+--- read_line
+--- @param f file*
+--- @returns string
+local read_line = function (f)
+  return f:read("l")
+end
+
+--- read_all
+---@param f file*
+---@return string
+local read_all = function (f)
+  return f:read("a")
+end
+
+--- read_all
+---@param f file*
+---@param length integer
+---@return any
+local read_bytes = function (f, length)
+  return f:read(length)
+end
+
+---========================================
+--- WRITER
+---========================================
+
+--- write_byte
+---@param f file*
+---@param value integer
+---@param signed boolean|nil
+---@return file*|nil
+local write_byte = function (f, value, signed)
+  local fmt = "=B"
+  if signed then fmt = "=b" end
+  return f:write(string.pack(fmt, value))
+end
+
+--- write_short
+---@param f file*
+---@param value integer
+---@param signed boolean|nil
+---@return file*|nil
+local write_short = function (f, value, signed)
+  local fmt = "=H"
+  if signed then fmt = "=h" end
+  return f:write(string.pack(fmt, value))
+end
+
+--- write_integer
+---@param f file*
+---@param value integer
+---@param signed boolean|nil
+---@return file*|nil
+local write_integer = function (f, value, signed)
+  local fmt = "=I"
+  if signed then fmt = "=i" end
+  return f:write(string.pack(fmt, value))
+end
+
+--- write_long
+---@param f file*
+---@param value integer
+---@param signed boolean|nil
+---@return file*|nil
+local write_long = function (f, value, signed)
+  local fmt = "=L"
+  if signed then fmt = "=l" end
+  return f:write(string.pack(fmt, value))
+end
+
+--- write_size
+---@param f file*
+---@param value integer
+---@param signed boolean|nil
+---@return file*|nil
+local write_size = function (f, value, signed)
+  local fmt = "=T" -- always the same!
+  return f:write(string.pack(fmt, value))
+end
+
+--- write_float
+---@param f file*
+---@param value number
+---@param double boolean|nil
+---@return file*|nil
+local write_float = function (f, value, double)
+  if double then
+    return f:write(string.pack("=d", value))
+  end
+  return f:write(string.pack("=f", value))
+end
+
+--- write_string
+---@param f file*
+---@param value number
+---@param size integer
+---@return file*|nil
+local write_string = function (f, value, size)
+  local fmt = "c" .. size
+  return f:write(string.pack(fmt, value))
+end
+
+--- write_all
+---@param f file*
+---@param value any
+---@return file*|nil
+local write_all = function (f, value)
+  return f:write(value)
+end
+
+---========================================
+--- MODULE
+---========================================
 
 return {
-  r_all = read_all,
-  w_all = write_all,
-  r_buf = read_buf,
-  r_chrs = read_chars,
-  w_chrs = write_chars,
-  r_cstr = read_c_str,
-  r_i32 = read_i32,
-  w_i32 = write_i32,
-  r_u8 = read_u8,
-  w_u8 = write_u8,
+  reader = {
+    number = {
+      byte = read_byte,
+      short = read_short,
+      integer = read_integer,
+      long = read_long,
+      size = read_size,
+      float = read_float,
+    },
+    string = read_string,
+    cstring = read_cstring,
+    line = read_line,
+    all = read_all,
+    bytes = read_bytes,
+  },
+  writer = {
+    number = {
+      byte = write_byte,
+      short = write_short,
+      integer = write_integer,
+      long = write_long,
+      size = write_size,
+      float = write_float,
+    },
+    string = write_string,
+    -- cstring = write_cstring, -- does it even make sense!
+    -- line = write_line, -- does it even make sense!
+    all = write_all,
+    -- bytes = write_bytes, -- does it even make sense!
+  }
 }
